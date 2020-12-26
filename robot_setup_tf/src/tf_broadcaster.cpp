ea776e5fb41ef629a9d9f69ea97c8c33d739605f
Raphael <benistan@etud.insa-toulouse.fr>
2020-12-26 16:01:13 +0100

    transfrom + fake PointCloud publisher + PointCloud listener and transformer and republisher
    PointCloud data are published on "laser" topic
    
    PointCloud data are transformed from "lidar_link" to "base_link"
    
    tranformed PointCloud data are published on "tf_laser" topic

--- robot_setup_tf/src/tf_broadcaster.cpp
+++ robot_setup_tf/src/tf_broadcaster.cpp
@@ -10,10 +10,11 @@ int main(int argc, char** argv){
   tf::TransformBroadcaster broadcaster;
 
   while(n.ok()){
+    //We create a broadcaster that will listen on ROS topics and wait for the call of a transformation from base_laser to base_link
     broadcaster.sendTransform(
       tf::StampedTransform(
-        tf::Transform(tf::Quaternion(0, 0, 0, 1), tf::Vector3(0.1, 0.0, 0.2)),
-        ros::Time::now(),"base_link", "base_laser"));
+        tf::Transform(tf::Quaternion(0, 0, 0, 1), tf::Vector3(-0.34, 0.0, 0.47)),
+        ros::Time::now(),"base_link", "base_laser")); //MODIFY WITH THE REAL OFFSET BETWEEN LASER AND CENTER OF THE CAR
     r.sleep();
   }
 }
--- robot_setup_tf/src/tf_listener.cpp
+++ robot_setup_tf/src/tf_listener.cpp
@@ -1,42 +1,50 @@
-#include <ros/ros.h>
-#include <geometry_msgs/PointStamped.h>
+#include "ros/ros.h"
+#include <sensor_msgs/PointCloud.h>
 #include <tf/transform_listener.h>
 
-void transformPoint(const tf::TransformListener& listener){
-  //we'll create a point in the base_laser frame that we'd like to transform to the base_link frame
-  geometry_msgs::PointStamped laser_point;
-  laser_point.header.frame_id = "base_laser";
+tf::TransformListener* p_listener;
+ros::Publisher* p_publisher;
 
-  //we'll just use the most recent transform available for our simple example
-  laser_point.header.stamp = ros::Time();
 
-  //just an arbitrary point in space
-  laser_point.point.x = 1.0;
-  laser_point.point.y = 0.2;
-  laser_point.point.z = 0.0;
-
-  try{
-    geometry_msgs::PointStamped base_point;
-    listener.transformPoint("base_link", laser_point, base_point);
+void LaserCallback(const sensor_msgs::PointCloud::ConstPtr& cloud_msg)
+{
+    ROS_INFO("I heard: [%.2f]", cloud_msg->points[0].x);
+  
+    try{
+    sensor_msgs::PointCloud base_cloud;
+    p_listener->transformPointCloud("base_link", ros::Time::now()-ros::Duration(0.02), *cloud_msg, "base_laser", base_cloud);
 
     ROS_INFO("base_laser: (%.2f, %.2f. %.2f) -----> base_link: (%.2f, %.2f, %.2f) at time %.2f",
-        laser_point.point.x, laser_point.point.y, laser_point.point.z,
-        base_point.point.x, base_point.point.y, base_point.point.z, base_point.header.stamp.toSec());
+       cloud_msg->points[0].x, cloud_msg->points[0].y, cloud_msg->points[0].z,
+       base_cloud.points[0].x, base_cloud.points[0].y, base_cloud.points[0].z, base_cloud.header.stamp.toSec());
+
+    p_publisher->publish(base_cloud);
+      
   }
   catch(tf::TransformException& ex){
     ROS_ERROR("Received an exception trying to transform a point from \"base_laser\" to \"base_link\": %s", ex.what());
   }
+  
+
 }
 
-int main(int argc, char** argv){
-  ros::init(argc, argv, "robot_tf_listener");
+int main(int argc, char **argv)
+{
+  ros::init(argc, argv, "listener");
+
   ros::NodeHandle n;
 
-  tf::TransformListener listener(ros::Duration(10));
-
-  //we'll transform a point once every second
-  ros::Timer timer = n.createTimer(ros::Duration(1.0), boost::bind(&transformPoint, boost::ref(listener)));
+  ros::Publisher transformed_cloud_pub = n.advertise<sensor_msgs::PointCloud>("tf_laser", 50); //Publisher for the transformed PointCloud
+  p_publisher = &transformed_cloud_pub;
+  
+  tf::TransformListener listener(ros::Duration(10)); //PointCloud Transform listener
+  p_listener = &listener;
+  
+  //We create our listener on the laser topic where PointClouds are published
+  //We will call and listen a transform every time a PointCloud is published
+  ros::Subscriber sub = n.subscribe("laser", 1000, LaserCallback);
 
   ros::spin();
 
+  return 0;
 }
